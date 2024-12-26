e8e2b96a0d40d9629aaa80fffb194ea4131b687a
EricsonCR <ericson4634@gmail.com>
2024-12-26 02:26:34 -0500

    package, properties, commit 1

---
+++ src/main/java/com/ericson/tiendasmartech/TiendaSmarTechApplication.java
@@ -0,0 +1,13 @@
+package com.ericson.tiendasmartech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSmarTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaSmarTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericson/tiendasmartech/TiendaSmarTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericson.tiendasmartech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSmarTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
